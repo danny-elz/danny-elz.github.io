46c1be1fc658ec9e762ba5aba2edddfe7e768e1a
Danny <109995999+danny-elz@users.noreply.github.com>
2025-03-03 21:17:01 -0500

    Visualization tweaks

--- script.js
+++ script.js
@@ -204,13 +204,6 @@ Type 'cat resume.txt' to view resume details`;
   });
 });
 
-function toggleMenu() {
-  const menu = document.querySelector(".menu-links");
-  const icon = document.querySelector(".hamburger-icon");
-  menu.classList.toggle("open");
-  icon.classList.toggle("open");
-}
-
 function handleScrollAnimations() {
   const observer = new IntersectionObserver((entries) => {
     entries.forEach(entry => {
